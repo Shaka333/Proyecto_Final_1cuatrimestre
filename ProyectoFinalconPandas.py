7e60cb9ec1d91fd6310be01994e4940545a84440
Shaka333 <cristopher.baezgonzalez@ucreativa.com>
2024-04-24 19:25:06 -0600

    Elimino codigo de mayor venta , por motivos de no uso

--- ProyectoFinalconPandas.py
+++ ProyectoFinalconPandas.py
@@ -32,8 +32,6 @@ class ControlVentaFresas:
     def agregar_inventario(self, cantidad):
         self.inventario_fresas += cantidad
 
-    def mayor_comprador(self):
-        return max(self.negocio1, self.negocio2, self.negocio3)
 
     def registrar_ventas_mes(self):
         self.ventas_mensuales.append(self.fresas_vendidas)
@@ -82,12 +80,11 @@ def main():
     while True:
         print("1. Ingresar venta")
         print("2. ¿Cuánto producto hay en bodega?")
-        print("3. ¿Cuál es el mayor comprador?")
-        print("4. ¿Cuál es la cantidad total de fresas vendidas?")
-        print("5. ¿Cuántas fresas desea agregar al inventario?")
-        print("6. Generar gráficos")
-        print("7. Generar reporte de ventas")
-        print("8. Salir")
+        print("3. ¿Cuál es la cantidad total de fresas vendidas?")
+        print("4. ¿Cuántas fresas desea agregar al inventario?")
+        print("5. Generar gráficos")
+        print("6. Generar reporte de ventas")
+        print("7. Salir")
 
         opcion = int(input("Ingrese su opción: "))
 
@@ -99,23 +96,42 @@ def main():
         elif opcion == 2:
             print("La cantidad total de fresas en inventario es de:", control_fresas.inventario_fresas)
         elif opcion == 3:
-            print("El mayor comprador es:", control_fresas.mayor_comprador())
-        elif opcion == 4:
             print("La cantidad de fresas vendidas es de:", control_fresas.fresas_vendidas)
-        elif opcion == 5:
+        elif opcion == 4:
             cantidad = int(input("¿Cuántas fresas desea agregar al inventario? "))
             control_fresas.agregar_inventario(cantidad)
             print("Se ha actualizado el inventario con éxito.")
             print("La cantidad total de fresas en inventario es de:", control_fresas.inventario_fresas)
+        elif opcion == 5:
+            while True:
+                print("1. Grafica vendedor del mes")
+                print("2. Grafico producto mas vendido")
+                print("3. Grafico producto menos vendido")
+                print("4. Grafico ventas del mes")
+                print("5. Salir al menu principal")
+
+                opcion1 = int(input("Ingrese su opción: "))
+
+                if opcion1 == 1:
+                    control_fresas.registrar_ventas_mes()
+                    control_fresas.grafico_vendedor_mes()
+                elif opcion1 == 2:
+                    control_fresas.registrar_ventas_mes()
+                    control_fresas.grafico_producto_mas_vendido()
+                elif opcion1 == 3:
+                    control_fresas.registrar_ventas_mes()
+                    control_fresas.grafico_producto_menos_vendido()
+                elif opcion1 == 4:
+                    control_fresas.registrar_ventas_mes()
+                    control_fresas.grafico_ventas_mes()
+                elif opcion == 5:
+                     print("Gracias por usar el sistema")
+                     break
+                else:
+                    print("Opción no válida. Intente de nuevo.")
         elif opcion == 6:
-            control_fresas.registrar_ventas_mes()
-            control_fresas.grafico_vendedor_mes()
-            control_fresas.grafico_producto_mas_vendido()
-            control_fresas.grafico_producto_menos_vendido()
-            control_fresas.grafico_ventas_mes()
-        elif opcion == 7:
             control_fresas.generar_reporte_ventas()
-        elif opcion == 8:
+        elif opcion == 7:
             print("Gracias por usar el sistema")
             break
         else:
